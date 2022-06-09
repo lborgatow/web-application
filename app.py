aabf0f0072823e74d11e829771b774f3959e32b6
Leonardo <lborgatow@gmail.com>
2022-06-09 13:18:40 +0000

    Adicionou bootstrap, criamos os templates e a passagem de dados para html

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 # Ao abrir o Gitpod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World"
+lugares = [
+    {"local": 'Fatec', "cidade": 'São José do Rio Preto', "pais": 'Brasil'},   
+    {"local": 'Torre Eifel', "cidade": 'Paris', "pais": 'França'},   
+]
 
-app.run()
+@app.route('/')
+def index():
+    return render_template('index.html', lugares=lugares)
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+app.run(debug=True)
