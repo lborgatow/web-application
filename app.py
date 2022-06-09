4ddc73751feadfb5c1bfcf378822bb9768afeb56
Leonardo <lborgatow@gmail.com>
2022-06-09 14:12:22 +0000

    Adicionou o template 'create.html' e as funções 'create' e 'save'

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # Ao abrir o Gitpod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from uuid import uuid4
 
 app = Flask(__name__)
 
 lugares = [
-    {"local": 'Fatec', "cidade": 'São José do Rio Preto', "pais": 'Brasil'},   
-    {"local": 'Torre Eifel', "cidade": 'Paris', "pais": 'França'},   
+    {"id": 1, "local": 'Fatec', "cidade": 'São José do Rio Preto', "pais": 'Brasil'},   
 ]
 
 @app.route('/')
@@ -17,5 +17,19 @@ def index():
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+@app.route('/save', methods=['POST'])
+def save():
+    local = request.form['local']     # <input name="..." ...
+    cidade = request.form['cidade']   # Sempre será uma string!
+    pais = request.form['pais']
+    lugares.append({"id": uuid4(), "local": local, "cidade": cidade, "pais": pais})
+    return render_template('index.html', lugares=lugares)
+
+def delete():
+    pass
+
+def update():
+    pass
 
 app.run(debug=True)
