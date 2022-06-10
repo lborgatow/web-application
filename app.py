4ff4c3b28d5696dc640cf5adf7db557ac3c5c787
Leonardo <lborgatow@gmail.com>
2022-06-10 15:01:56 +0000

    Adicionou as funções 'save_file' e 'delete' e o arquivo 'lugares.csv'

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 
 from flask import Flask, render_template, request
 from uuid import uuid4
+import csv
 
 app = Flask(__name__)
 
-lugares = [
-    {"id": 1, "local": 'Fatec', "cidade": 'São José do Rio Preto', "pais": 'Brasil'},   
-]
+f_in = open('lugares.csv')
+leitor = csv.DictReader(f_in, ['id', 'local', 'cidade', 'pais'])
+lugares = [linha for linha in leitor]
 
 @app.route('/')
 def index():
@@ -18,16 +19,31 @@ def index():
 def create():
     return render_template('create.html')
 
+def save_file():
+    with open('lugares.csv', 'w') as f_out:
+        escritor = csv.DictWriter(f_out, ['id', 'local', 'cidade', 'pais'])
+        escritor.writerows(lugares)
+
 @app.route('/save', methods=['POST'])
 def save():
     local = request.form['local']     # <input name="..." ...
     cidade = request.form['cidade']   # Sempre será uma string!
     pais = request.form['pais']
     lugares.append({"id": uuid4(), "local": local, "cidade": cidade, "pais": pais})
+
+    save_file()
+
     return render_template('index.html', lugares=lugares)
 
-def delete():
-    pass
+@app.route('/delete/<id>')
+def delete(id):     
+    for lugar in lugares:
+        if lugar['id'] == id:
+            lugares.remove(lugar)
+
+    save_file()
+
+    return render_template('index.html', lugares=lugares)
 
 def update():
     pass
