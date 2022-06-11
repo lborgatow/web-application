09e0212d4e78592f946cc24aa61110e104dac1d0
Leonardo <lborgatow@gmail.com>
2022-06-11 18:03:09 +0000

    Adicionou as funções 'edit' e 'update' e atualizou o restante

--- app.py
+++ app.py
@@ -1,51 +1,71 @@
-# Ao abrir o Gitpod, execute:
+# Ao abrir o GitPod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from uuid import uuid4
 import csv
 
 app = Flask(__name__)
 
-f_in = open('lugares.csv')
-leitor = csv.DictReader(f_in, ['id', 'local', 'cidade', 'pais'])
-lugares = [linha for linha in leitor]
+
+lugares = []
+
+with open('lugares.csv', 'r') as file_in:
+    leitor = csv.DictReader(file_in)
+    for lugar in leitor:
+        lugares.append(dict(lugar))
+
+
+def save_file():
+    with open('lugares.csv', 'w') as file_out:
+        escritor = csv.DictWriter(file_out, ['id', 'Local', 'Cidade', 'País'])
+        escritor.writeheader()
+        escritor.writerows(lugares)
 
 @app.route('/')
 def index():
-    return render_template('index.html', lugares=lugares)
+    save_file()
+    return render_template('index.html', lugares=lugares) 
 
 @app.route('/create')
 def create():
     return render_template('create.html')
 
-def save_file():
-    with open('lugares.csv', 'w') as f_out:
-        escritor = csv.DictWriter(f_out, ['id', 'local', 'cidade', 'pais'])
-        escritor.writerows(lugares)
-
 @app.route('/save', methods=['POST'])
 def save():
-    local = request.form['local']     # <input name="..." ...
-    cidade = request.form['cidade']   # Sempre será uma string!
-    pais = request.form['pais']
-    lugares.append({"id": uuid4(), "local": local, "cidade": cidade, "pais": pais})
+    local = request.form['Local']
+    cidade = request.form['Cidade']
+    pais = request.form['País']
+   
+    lugares.append({'id': uuid4(), 'Local': local, 'Cidade': cidade, 'País': pais})
+    
+    return redirect('/')
 
-    save_file()
+@app.route('/edit/<id>')
+def edit(id):
+    for lugar in lugares:
+        id_string = str(lugar['id'])
+        if id_string == id: 
+            return render_template('edit.html', lugar=lugar)
 
-    return render_template('index.html', lugares=lugares)
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    for lugar in lugares:
+        id_string = str(lugar['id'])
+        if id_string == id:
+            lugar['Local'] = request.form['Local']
+            lugar['Cidade'] = request.form['Cidade']
+            lugar['País'] = request.form['País']
+    
+    return redirect('/')
 
 @app.route('/delete/<id>')
-def delete(id):     
+def delete(id):
     for lugar in lugares:
-        if lugar['id'] == id:
-            lugares.remove(lugar)
+        id_string = str(lugar['id'])
+        if id_string == id:
+            del lugares[lugares.index(lugar)]
+            return render_template('deleted.html')
 
-    save_file()
-
-    return render_template('index.html', lugares=lugares)
-
-def update():
-    pass
 
 app.run(debug=True)
