758ddf6680ee3b01486deab6e0a1ed04b0640a0c
Leonardo <lborgatow@gmail.com>
2022-06-13 18:01:20 +0000

    Resolução de bugs / Versão final

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect
 from uuid import uuid4
 import csv
 
+
 app = Flask(__name__)
 
 
@@ -13,26 +14,38 @@ lugares = []
 with open('lugares.csv', 'r') as file_in:
     leitor = csv.DictReader(file_in)
     for lugar in leitor:
-        lugares.append(dict(lugar))
+        lugares.append(lugar)
 
 
 def save_file():
+    """Salva as informações no arquivo lugares.csv"""
+
     with open('lugares.csv', 'w') as file_out:
         escritor = csv.DictWriter(file_out, ['id', 'Local', 'Cidade', 'País'])
         escritor.writeheader()
         escritor.writerows(lugares)
 
+
 @app.route('/')
 def index():
+    """Salva o lugar e retorna a página index.html"""
+
     save_file()
     return render_template('index.html', lugares=lugares) 
 
+
 @app.route('/create')
 def create():
+    """Retorna a página create.html"""
+
     return render_template('create.html')
+
 
 @app.route('/save', methods=['POST'])
 def save():
+    """Atribui nas variáveis as informações inseridas e adiciona um novo dicionário
+    na lista de lugares"""
+
     local = request.form['Local']
     cidade = request.form['Cidade']
     pais = request.form['País']
@@ -41,31 +54,41 @@ def save():
     
     return redirect('/')
 
+
 @app.route('/edit/<id>')
 def edit(id):
+    """Retorna a página edit.html de acordo com o id selecionado"""
+
     for lugar in lugares:
         id_string = str(lugar['id'])
         if id_string == id: 
             return render_template('edit.html', lugar=lugar)
 
+
 @app.route('/update/<id>', methods=['POST'])
 def update(id):
+    """Altera as informações de acordo com as novas informações inseridas
+    no id selecionado"""
+
     for lugar in lugares:
         id_string = str(lugar['id'])
-        if id_string == id:
+        if id_string == id: 
             lugar['Local'] = request.form['Local']
             lugar['Cidade'] = request.form['Cidade']
             lugar['País'] = request.form['País']
     
     return redirect('/')
 
+
 @app.route('/delete/<id>')
 def delete(id):
+    """Apaga as informações de acordo com o id selecionado"""
+
     for lugar in lugares:
         id_string = str(lugar['id'])
-        if id_string == id:
+        if id_string == id: 
             del lugares[lugares.index(lugar)]
-            return render_template('deleted.html')
+            return redirect('/')
 
 
 app.run(debug=True)
